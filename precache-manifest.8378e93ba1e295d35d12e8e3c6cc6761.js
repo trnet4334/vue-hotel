1bc910178fec41fa3cf2a44c5b6a329051eb1cfc
trnet4334 <search0210@gmail.com>
2020-10-24 21:05:48 +0800

    Deploy

---
+++ precache-manifest.8378e93ba1e295d35d12e8e3c6cc6761.js
@@ -0,0 +1,586 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6827e8ab9d947b07ad",
+    "url": "/vue-hotel/css/app.f0cbdd31.css"
+  },
+  {
+    "revision": "3696338f7411f03768d1",
+    "url": "/vue-hotel/css/chunk-07fcad7f.81888589.css"
+  },
+  {
+    "revision": "49d725ce63b8c25793fe",
+    "url": "/vue-hotel/css/chunk-0c9bb8f9.e815906e.css"
+  },
+  {
+    "revision": "3aef8cee58e66705bade",
+    "url": "/vue-hotel/css/chunk-1224985d.d0966c59.css"
+  },
+  {
+    "revision": "563158faded826363138",
+    "url": "/vue-hotel/css/chunk-1b6cf852.520b89ab.css"
+  },
+  {
+    "revision": "c90c5cb25aac5c21df0e",
+    "url": "/vue-hotel/css/chunk-1cdc2f58.0136f1d2.css"
+  },
+  {
+    "revision": "5bb4aa4b0612c57b9a81",
+    "url": "/vue-hotel/css/chunk-21ff5270.a1d0973a.css"
+  },
+  {
+    "revision": "92177a1e07582626b3cb",
+    "url": "/vue-hotel/css/chunk-30a8cfa8.7c91c197.css"
+  },
+  {
+    "revision": "aee44b5ffa1419db74e8",
+    "url": "/vue-hotel/css/chunk-3141154a.a56a0aef.css"
+  },
+  {
+    "revision": "112eceed5d25d292f958",
+    "url": "/vue-hotel/css/chunk-33186fe8.e5dda0bc.css"
+  },
+  {
+    "revision": "4fc9978b89ace57c770c",
+    "url": "/vue-hotel/css/chunk-3c83e774.870ae7aa.css"
+  },
+  {
+    "revision": "8dd7cae41b6e80d6eaa7",
+    "url": "/vue-hotel/css/chunk-3cc0e584.02cd8861.css"
+  },
+  {
+    "revision": "a335c2a584fc1722cebc",
+    "url": "/vue-hotel/css/chunk-3de42a2c.8420ea50.css"
+  },
+  {
+    "revision": "55f9f5bf5157a8923e73",
+    "url": "/vue-hotel/css/chunk-3f139b92.5ac40cd9.css"
+  },
+  {
+    "revision": "9623fba2466066cd821d",
+    "url": "/vue-hotel/css/chunk-4969c8dc.8bb1ecee.css"
+  },
+  {
+    "revision": "d01d09bf04606f896aae",
+    "url": "/vue-hotel/css/chunk-498f4481.b212747b.css"
+  },
+  {
+    "revision": "b42088e365172dfc9c37",
+    "url": "/vue-hotel/css/chunk-56c71fb4.d63f71b4.css"
+  },
+  {
+    "revision": "4cb1c94402904ca83c8d",
+    "url": "/vue-hotel/css/chunk-5c96269f.90cae4dd.css"
+  },
+  {
+    "revision": "53e1b8b1e71a4ba12e73",
+    "url": "/vue-hotel/css/chunk-62ea0530.9a48b5e4.css"
+  },
+  {
+    "revision": "fe787d8c74f0f5393282",
+    "url": "/vue-hotel/css/chunk-69a2d5f4.436a4a18.css"
+  },
+  {
+    "revision": "084364cd8566c7dc00da",
+    "url": "/vue-hotel/css/chunk-6ad8ad53.754b4625.css"
+  },
+  {
+    "revision": "e003e30a6aecffbc59ba",
+    "url": "/vue-hotel/css/chunk-77e3f1d6.a0429529.css"
+  },
+  {
+    "revision": "68b5f97f653f0df756e1",
+    "url": "/vue-hotel/css/chunk-7e2ca2a6.d919ad51.css"
+  },
+  {
+    "revision": "5f6bf6552f5ff219a305",
+    "url": "/vue-hotel/css/chunk-80879900.f874a366.css"
+  },
+  {
+    "revision": "47e377ab4090d817d282",
+    "url": "/vue-hotel/css/chunk-81574b08.a818fcfa.css"
+  },
+  {
+    "revision": "3382baf36c23b9e02c20",
+    "url": "/vue-hotel/css/chunk-9509e53c.6a24d228.css"
+  },
+  {
+    "revision": "885bdf1fe267f4e9abe6",
+    "url": "/vue-hotel/css/chunk-9fc3b2dc.3c0586fc.css"
+  },
+  {
+    "revision": "75c5b7ee3ac57b8b3cc4",
+    "url": "/vue-hotel/css/chunk-a4fd3cfe.94299c07.css"
+  },
+  {
+    "revision": "f2339ba11f6180a682b4",
+    "url": "/vue-hotel/css/chunk-b30c8f18.3cfc1478.css"
+  },
+  {
+    "revision": "36dd2a6a388f486522f3",
+    "url": "/vue-hotel/css/chunk-d2779242.38e561f2.css"
+  },
+  {
+    "revision": "bb655920f06695220e99",
+    "url": "/vue-hotel/css/chunk-e43e2a02.9635898a.css"
+  },
+  {
+    "revision": "b6f7455f2aa9e85359b3",
+    "url": "/vue-hotel/css/chunk-f76f9eea.6fe1b300.css"
+  },
+  {
+    "revision": "7a7e4c6ae60e7997a106",
+    "url": "/vue-hotel/css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-hotel/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-hotel/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "0c8dcce5571d7f15ac7df73171790acd",
+    "url": "/vue-hotel/img/accessible-img-2.0c8dcce5.jpg"
+  },
+  {
+    "revision": "e0087b8db45b4cdbb790949861bf40af",
+    "url": "/vue-hotel/img/accessible-img-3.e0087b8d.jpg"
+  },
+  {
+    "revision": "89aa7ad24952417070dab59810b8e0c5",
+    "url": "/vue-hotel/img/accessible-img.89aa7ad2.jpg"
+  },
+  {
+    "revision": "5f83821be526933d9bca2359710a23ae",
+    "url": "/vue-hotel/img/background-1.5f83821b.jpg"
+  },
+  {
+    "revision": "14702498c25635b96e6ecdc1aa8a84cf",
+    "url": "/vue-hotel/img/background-2.14702498.jpg"
+  },
+  {
+    "revision": "7d7164e9423e598c4972469d11a0d62e",
+    "url": "/vue-hotel/img/background-img.7d7164e9.jpg"
+  },
+  {
+    "revision": "5d03663a07ebe102673920a7b61e8419",
+    "url": "/vue-hotel/img/bar.5d03663a.jpg"
+  },
+  {
+    "revision": "de2bdcfa71ea5e5a535f0c56012e062c",
+    "url": "/vue-hotel/img/basket-essential.de2bdcfa.jpg"
+  },
+  {
+    "revision": "b3e26fcf860ab39eec47be3009bd069b",
+    "url": "/vue-hotel/img/bedtime-snacks.b3e26fcf.jpg"
+  },
+  {
+    "revision": "3313399df2473b81056f47e5343a0f9c",
+    "url": "/vue-hotel/img/careers-img-1.3313399d.jpg"
+  },
+  {
+    "revision": "e10c01d293499534415534a79a2c77bb",
+    "url": "/vue-hotel/img/careers-img-2.e10c01d2.jpg"
+  },
+  {
+    "revision": "c5b5a3e4a0d3b33390a053f9497ba59d",
+    "url": "/vue-hotel/img/champagne.c5b5a3e4.jpg"
+  },
+  {
+    "revision": "fa1689bf2456d225ed1e380be8b8be86",
+    "url": "/vue-hotel/img/classic-guestroom-img.fa1689bf.jpg"
+  },
+  {
+    "revision": "e5a279dac47d561cb5352b58cd05e095",
+    "url": "/vue-hotel/img/club-img.e5a279da.jpg"
+  },
+  {
+    "revision": "36bf941bdfb6e1ca1d2ff06fb7cc9cf5",
+    "url": "/vue-hotel/img/contact-img.36bf941b.jpg"
+  },
+  {
+    "revision": "c188a30486240419b9eb209d1158678a",
+    "url": "/vue-hotel/img/deluxe-guestroom-img.c188a304.jpg"
+  },
+  {
+    "revision": "a9945b91fd17fa872b6d5616251ecc3a",
+    "url": "/vue-hotel/img/dining.a9945b91.jpg"
+  },
+  {
+    "revision": "aba01bf1810fab7cc7a24f6b8fcd8cbd",
+    "url": "/vue-hotel/img/dinning-img-1.aba01bf1.jpg"
+  },
+  {
+    "revision": "f2d6069ecead03cc404820c35e78b23a",
+    "url": "/vue-hotel/img/dinning-img-2.f2d6069e.jpg"
+  },
+  {
+    "revision": "17dc50381be88a6c635f2a6f7eed9a0c",
+    "url": "/vue-hotel/img/dinning-img.17dc5038.jpg"
+  },
+  {
+    "revision": "1db0320b7a5bd72cf34b04e178d5765f",
+    "url": "/vue-hotel/img/events-img.1db0320b.jpg"
+  },
+  {
+    "revision": "94e0f539877cde683c0a44ec1b52ec70",
+    "url": "/vue-hotel/img/faq-img.94e0f539.jpg"
+  },
+  {
+    "revision": "295d84b3a2590143a8f66e2f3de54934",
+    "url": "/vue-hotel/img/header.295d84b3.jpg"
+  },
+  {
+    "revision": "bbfda897b3f9daa5b44fcf64af2895e5",
+    "url": "/vue-hotel/img/hotel.bbfda897.jpg"
+  },
+  {
+    "revision": "4517ebd8889537dddcf3a22e866536d3",
+    "url": "/vue-hotel/img/luxury-suite-img.4517ebd8.jpg"
+  },
+  {
+    "revision": "fd3e696e7458549559c4c24cfd878c1d",
+    "url": "/vue-hotel/img/meeting-carousel-1.fd3e696e.jpg"
+  },
+  {
+    "revision": "9f50118ca71fce3df09bf5fa0a12d8a1",
+    "url": "/vue-hotel/img/meeting-carousel-2.9f50118c.jpg"
+  },
+  {
+    "revision": "9884e9744beecf5985a6e4fc1b744a57",
+    "url": "/vue-hotel/img/meeting-carousel-3.9884e974.jpg"
+  },
+  {
+    "revision": "9deca35615366dec051ec45db18bd5ee",
+    "url": "/vue-hotel/img/meeting-carousel-4.9deca356.jpg"
+  },
+  {
+    "revision": "a2e5f151a80557d38e2c6c7c322ecf6d",
+    "url": "/vue-hotel/img/meeting-img-1.a2e5f151.jpg"
+  },
+  {
+    "revision": "7f8924a4167ce1afe86b7254272a1b65",
+    "url": "/vue-hotel/img/meeting-img-2.7f8924a4.jpg"
+  },
+  {
+    "revision": "032f7d9b5cdc0295799366fca08bb332",
+    "url": "/vue-hotel/img/meeting-img-3.032f7d9b.jpg"
+  },
+  {
+    "revision": "52efe6b7fac3fa0bef578d71deda0f35",
+    "url": "/vue-hotel/img/menu-img.52efe6b7.jpg"
+  },
+  {
+    "revision": "caa078866be639421bfa9693c2177562",
+    "url": "/vue-hotel/img/outdoor.caa07886.jpg"
+  },
+  {
+    "revision": "132153334b6afce6654f250d16216da4",
+    "url": "/vue-hotel/img/pet-friendly-img-2.13215333.jpg"
+  },
+  {
+    "revision": "1c67b746d62eaef2ee22a9ed8d3fed93",
+    "url": "/vue-hotel/img/pet-friendly-img.1c67b746.jpg"
+  },
+  {
+    "revision": "bae67cc67232b337b8e7a039e061c3cc",
+    "url": "/vue-hotel/img/pool-bar-img.bae67cc6.jpg"
+  },
+  {
+    "revision": "5e193032949846b8d2219bc41200f568",
+    "url": "/vue-hotel/img/private-deck.5e193032.jpg"
+  },
+  {
+    "revision": "1407c94d4507e9d7bb6bd43960464919",
+    "url": "/vue-hotel/img/room-carousel-1.1407c94d.jpg"
+  },
+  {
+    "revision": "cf386499a57cb5efdedac5d28a1f3f19",
+    "url": "/vue-hotel/img/room-carousel-2.cf386499.jpg"
+  },
+  {
+    "revision": "7e87bb0181d63f9ba156a94b8dd829f2",
+    "url": "/vue-hotel/img/room-carousel-3.7e87bb01.jpg"
+  },
+  {
+    "revision": "03d828c4ce4941ea96a5575f735c4028",
+    "url": "/vue-hotel/img/room-carousel-4.03d828c4.jpg"
+  },
+  {
+    "revision": "74929765df3d0ca79dfa40b1206712fb",
+    "url": "/vue-hotel/img/room-carousel-5.74929765.jpg"
+  },
+  {
+    "revision": "eff170cdef46456de8202b0a2899c4b9",
+    "url": "/vue-hotel/img/room-carousel-6.eff170cd.jpg"
+  },
+  {
+    "revision": "50be5e34fb916b2bbb024deedd6b20b6",
+    "url": "/vue-hotel/img/room-img.50be5e34.jpg"
+  },
+  {
+    "revision": "ae44d0258e0945aae43190511a00adce",
+    "url": "/vue-hotel/img/room.ae44d025.jpg"
+  },
+  {
+    "revision": "f1597c7ef52c19e44ca53586d2d7fc6c",
+    "url": "/vue-hotel/img/rose.f1597c7e.jpg"
+  },
+  {
+    "revision": "b3886f628668dddb400be23115c20e77",
+    "url": "/vue-hotel/img/sparkling-water-img.b3886f62.jpg"
+  },
+  {
+    "revision": "1a9e9738920ea51169d162c841348900",
+    "url": "/vue-hotel/img/special-img-3.1a9e9738.jpg"
+  },
+  {
+    "revision": "ef34987f82bbb093c6d1184bdbe3871e",
+    "url": "/vue-hotel/img/specials-1.ef34987f.jpg"
+  },
+  {
+    "revision": "b6c7e4cb76029dbb75d155840412d7ae",
+    "url": "/vue-hotel/img/specials-2.b6c7e4cb.jpg"
+  },
+  {
+    "revision": "71d181ef462831a2f7d949284952dfb0",
+    "url": "/vue-hotel/img/specials-3.71d181ef.jpg"
+  },
+  {
+    "revision": "5ca024975b1b57ea75a62de9260645ec",
+    "url": "/vue-hotel/img/specials-img-1.5ca02497.jpg"
+  },
+  {
+    "revision": "aa7fb4095d496979f4dce43591c03850",
+    "url": "/vue-hotel/img/specials-img-2.aa7fb409.jpg"
+  },
+  {
+    "revision": "ef34987f82bbb093c6d1184bdbe3871e",
+    "url": "/vue-hotel/img/specials-img-4.ef34987f.jpg"
+  },
+  {
+    "revision": "b6c7e4cb76029dbb75d155840412d7ae",
+    "url": "/vue-hotel/img/specials-img-5.b6c7e4cb.jpg"
+  },
+  {
+    "revision": "71d181ef462831a2f7d949284952dfb0",
+    "url": "/vue-hotel/img/specials-img-6.71d181ef.jpg"
+  },
+  {
+    "revision": "59ee205fd74466338454f88cd67c3c4c",
+    "url": "/vue-hotel/img/story-img-1.59ee205f.jpg"
+  },
+  {
+    "revision": "43462517d61446fce03e5cbaadb58482",
+    "url": "/vue-hotel/img/story-img-2.43462517.jpg"
+  },
+  {
+    "revision": "36844563b10fce72fc324f7bcaa8a624",
+    "url": "/vue-hotel/img/sunday-brunch-img.36844563.jpg"
+  },
+  {
+    "revision": "01f8d6a7252f053e4e923561de0118a6",
+    "url": "/vue-hotel/img/the-spa-suite-img.01f8d6a7.jpg"
+  },
+  {
+    "revision": "e70232c3d7f321af7301c164e2f9e5b5",
+    "url": "/vue-hotel/img/wedding-carousel-1.e70232c3.jpg"
+  },
+  {
+    "revision": "46f31c1a5e43d7e6234605fe41bfc69f",
+    "url": "/vue-hotel/img/wedding-carousel-2.46f31c1a.jpg"
+  },
+  {
+    "revision": "f7e43d8bafe849c52e17be7a960fbab6",
+    "url": "/vue-hotel/img/wedding-carousel-3.f7e43d8b.jpg"
+  },
+  {
+    "revision": "3d03ed2ba609edc21cc71ebe64f44c56",
+    "url": "/vue-hotel/img/wedding-carousel-4.3d03ed2b.jpg"
+  },
+  {
+    "revision": "6606f76be2b9d9ca042d415175246837",
+    "url": "/vue-hotel/img/wedding-carousel-5.6606f76b.jpg"
+  },
+  {
+    "revision": "620d2d76fce0d7933b0f8a0c7a3159a3",
+    "url": "/vue-hotel/img/wedding-img-1.620d2d76.jpg"
+  },
+  {
+    "revision": "e97b077f496f4db225881340689ad372",
+    "url": "/vue-hotel/img/wedding-img-2.e97b077f.jpg"
+  },
+  {
+    "revision": "ed70e6d2482b1908b6d37aafc38baac0",
+    "url": "/vue-hotel/img/wedding-img-3.ed70e6d2.jpg"
+  },
+  {
+    "revision": "770a507643a0831f4042892acb9aab34",
+    "url": "/vue-hotel/img/wedding-img-4.770a5076.jpg"
+  },
+  {
+    "revision": "5ef7db2ef90aacf85e6389b556943665",
+    "url": "/vue-hotel/img/wedding-img.5ef7db2e.jpg"
+  },
+  {
+    "revision": "8ea92c134af89e36491c620351764628",
+    "url": "/vue-hotel/img/wellness-img-1.8ea92c13.jpg"
+  },
+  {
+    "revision": "7514db52ebedf8e3dacc434e520d1f88",
+    "url": "/vue-hotel/img/wellness-img-2.7514db52.jpg"
+  },
+  {
+    "revision": "d22f76dcca49a3f8d5df8ebf844166ab",
+    "url": "/vue-hotel/index.html"
+  },
+  {
+    "revision": "1c6827e8ab9d947b07ad",
+    "url": "/vue-hotel/js/app.c770a566.js"
+  },
+  {
+    "revision": "3696338f7411f03768d1",
+    "url": "/vue-hotel/js/chunk-07fcad7f.90417418.js"
+  },
+  {
+    "revision": "49d725ce63b8c25793fe",
+    "url": "/vue-hotel/js/chunk-0c9bb8f9.81b50775.js"
+  },
+  {
+    "revision": "3aef8cee58e66705bade",
+    "url": "/vue-hotel/js/chunk-1224985d.5f9bc6c7.js"
+  },
+  {
+    "revision": "563158faded826363138",
+    "url": "/vue-hotel/js/chunk-1b6cf852.92e36e8e.js"
+  },
+  {
+    "revision": "c90c5cb25aac5c21df0e",
+    "url": "/vue-hotel/js/chunk-1cdc2f58.76dffaa1.js"
+  },
+  {
+    "revision": "5bb4aa4b0612c57b9a81",
+    "url": "/vue-hotel/js/chunk-21ff5270.e997f873.js"
+  },
+  {
+    "revision": "fcec5e9e83aa0cd5bccb",
+    "url": "/vue-hotel/js/chunk-2d0e1d50.ceb71f77.js"
+  },
+  {
+    "revision": "92177a1e07582626b3cb",
+    "url": "/vue-hotel/js/chunk-30a8cfa8.6043d73f.js"
+  },
+  {
+    "revision": "aee44b5ffa1419db74e8",
+    "url": "/vue-hotel/js/chunk-3141154a.e31ebf4f.js"
+  },
+  {
+    "revision": "112eceed5d25d292f958",
+    "url": "/vue-hotel/js/chunk-33186fe8.da516023.js"
+  },
+  {
+    "revision": "4fc9978b89ace57c770c",
+    "url": "/vue-hotel/js/chunk-3c83e774.3bc4e1c7.js"
+  },
+  {
+    "revision": "8dd7cae41b6e80d6eaa7",
+    "url": "/vue-hotel/js/chunk-3cc0e584.3b3a566c.js"
+  },
+  {
+    "revision": "a335c2a584fc1722cebc",
+    "url": "/vue-hotel/js/chunk-3de42a2c.0b80ba20.js"
+  },
+  {
+    "revision": "55f9f5bf5157a8923e73",
+    "url": "/vue-hotel/js/chunk-3f139b92.f1deeebc.js"
+  },
+  {
+    "revision": "9623fba2466066cd821d",
+    "url": "/vue-hotel/js/chunk-4969c8dc.f051a512.js"
+  },
+  {
+    "revision": "d01d09bf04606f896aae",
+    "url": "/vue-hotel/js/chunk-498f4481.12b54593.js"
+  },
+  {
+    "revision": "b42088e365172dfc9c37",
+    "url": "/vue-hotel/js/chunk-56c71fb4.1da10fca.js"
+  },
+  {
+    "revision": "4cb1c94402904ca83c8d",
+    "url": "/vue-hotel/js/chunk-5c96269f.a5e2d395.js"
+  },
+  {
+    "revision": "53e1b8b1e71a4ba12e73",
+    "url": "/vue-hotel/js/chunk-62ea0530.813d122a.js"
+  },
+  {
+    "revision": "fe787d8c74f0f5393282",
+    "url": "/vue-hotel/js/chunk-69a2d5f4.13b8d6b4.js"
+  },
+  {
+    "revision": "084364cd8566c7dc00da",
+    "url": "/vue-hotel/js/chunk-6ad8ad53.d4828be7.js"
+  },
+  {
+    "revision": "e003e30a6aecffbc59ba",
+    "url": "/vue-hotel/js/chunk-77e3f1d6.7d7ad9b5.js"
+  },
+  {
+    "revision": "68b5f97f653f0df756e1",
+    "url": "/vue-hotel/js/chunk-7e2ca2a6.d19976cd.js"
+  },
+  {
+    "revision": "5f6bf6552f5ff219a305",
+    "url": "/vue-hotel/js/chunk-80879900.28fd53e1.js"
+  },
+  {
+    "revision": "47e377ab4090d817d282",
+    "url": "/vue-hotel/js/chunk-81574b08.413067be.js"
+  },
+  {
+    "revision": "3382baf36c23b9e02c20",
+    "url": "/vue-hotel/js/chunk-9509e53c.dc8f67eb.js"
+  },
+  {
+    "revision": "885bdf1fe267f4e9abe6",
+    "url": "/vue-hotel/js/chunk-9fc3b2dc.c313662c.js"
+  },
+  {
+    "revision": "75c5b7ee3ac57b8b3cc4",
+    "url": "/vue-hotel/js/chunk-a4fd3cfe.8ec1bf24.js"
+  },
+  {
+    "revision": "f2339ba11f6180a682b4",
+    "url": "/vue-hotel/js/chunk-b30c8f18.e4b6eb8a.js"
+  },
+  {
+    "revision": "7ba98ab0c75adb0e490e",
+    "url": "/vue-hotel/js/chunk-ba6bcdac.e4ecea79.js"
+  },
+  {
+    "revision": "36dd2a6a388f486522f3",
+    "url": "/vue-hotel/js/chunk-d2779242.0f1104ad.js"
+  },
+  {
+    "revision": "bb655920f06695220e99",
+    "url": "/vue-hotel/js/chunk-e43e2a02.0c6421ef.js"
+  },
+  {
+    "revision": "b6f7455f2aa9e85359b3",
+    "url": "/vue-hotel/js/chunk-f76f9eea.ef92ec75.js"
+  },
+  {
+    "revision": "7a7e4c6ae60e7997a106",
+    "url": "/vue-hotel/js/chunk-vendors.dea3004b.js"
+  },
+  {
+    "revision": "b724999dcba344a72767a434c5395712",
+    "url": "/vue-hotel/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-hotel/robots.txt"
+  }
+]);
