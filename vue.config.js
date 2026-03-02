1e6a2b51bd816eb15fff23301aea073ba732a443
Steven Yu <stevy@StevendeMini.localdomain>
2026-03-02 10:42:08 +0800

    fix: replace disableHostCheck with allowedHosts in devServer config

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
   devServer: {
     proxy: 'http://localhost:3000',
     headers: { 'Access-Control-Allow-Origin': '*' },
-    disableHostCheck: true
+    allowedHosts: 'all'
   },
 
   chainWebpack: config => {
